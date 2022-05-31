1e950bd66306dc2d9bf4c5177a660e22c71cfb25
khanshorif331 <khanshorif331@gmail.com>
2022-05-31 21:48:57 +0600

    getstaticpath made froom docs

--- pages/users/[id].js
+++ pages/users/[id].js
@@ -9,3 +9,19 @@ const User = () => {
 }
 
 export default User
+
+// This function gets called at build time
+export async function getStaticPaths() {
+	// Call an external API endpoint to get posts
+	const res = await fetch('https://jsonplaceholder.typicode.com/users')
+	const users = await res.json()
+
+	// Get the paths we want to pre-render based on posts
+	const paths = users.map(user => ({
+		params: { id: user.id },
+	}))
+
+	// We'll pre-render only these paths at build time.
+	// { fallback: false } means other routes should 404.
+	return { paths, fallback: false }
+}
